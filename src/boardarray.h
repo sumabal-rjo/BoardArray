6f5c2af6206a7bfb6f6a683405c7a98940c3a8d5
Romryl John O. Sumabal <sumabalromryljohno@gmail.com>
2025-09-20 13:20:56 +0800

    added functions to add()

--- src/boardarray.h
+++ src/boardarray.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "board.h"
+
 #define SIZE 5
 using namespace std;
 
@@ -16,7 +17,33 @@ class BoardArray : public Board {
         void add(Entry* entry) {
             // TODO: IMPLEMENT THIS FUNCTION
             // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
-            
+            int pos = -1;
+
+            for (int i = 0; i < index; i++) {
+                if (entry->compare(&array[i])) {
+                    pos = i;
+                    break;
+                }
+            }
+
+            if (pos == -1) {
+                    if (index < SIZE) {
+                        array[index++] = *entry;
+                    } else {
+                        cout << entry->name << "'s score is too low to be added!" << endl;
+                    }
+                    return;
+                }
+ 
+                if (index < SIZE) {
+                    index++;
+                }
+ 
+                for (int i = index - 1; i > pos; i--) {
+                    array[i] = array[i - 1];
+                }
+ 
+                array[pos] = *entry;
         }
 
         void print() {
@@ -25,4 +52,5 @@ class BoardArray : public Board {
                 array[i].print();
             }
         }
+
 };
